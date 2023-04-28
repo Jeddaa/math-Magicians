91b4c82f322a185f5dbf838a900a9f595ed07067
jeddaa <fathiato@gmail.com>
2023-04-28 08:30:33 +0100

    adding logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,75 @@
+import React, { useState } from 'react';
 import Display from './Display';
+import calculate from '../logic/calculate';
 
 export default function Calculator() {
+  const [value, setValue] = useState({ total: '0', next: null, operation: null });
+
+  function handleClick(e) {
+    setValue((value) => ({
+      ...value,
+      ...calculate(value, e.target.name),
+    }));
+  }
+
   return (
     <div className="calculator-div">
-      <Display />
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button type="button" className="orange">
+      <Display value={value} />
+      <button type="button" onClick={handleClick} name="AC">
+        AC
+      </button>
+      <button type="button" onClick={handleClick} name=" +/-">
+        +/-
+      </button>
+      <button type="button" onClick={handleClick} name="%">
+        %
+      </button>
+      <button type="button" className="orange" name="÷">
         ÷
       </button>
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button" className="orange">
+      <button type="button" onClick={handleClick} name="7">
+        7
+      </button>
+      <button type="button" onClick={handleClick} name="8">
+        8
+      </button>
+      <button type="button" onClick={handleClick} name="9">
+        9
+      </button>
+      <button type="button" className="orange" onClick={handleClick} name="x">
         x
       </button>
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button" className="orange">
+      <button type="button" onClick={handleClick} name="4">
+        4
+      </button>
+      <button type="button" onClick={handleClick} name="5">
+        5
+      </button>
+      <button type="button" onClick={handleClick} name="6">
+        6
+      </button>
+      <button type="button" className="orange" onClick={handleClick} name="-">
         -
       </button>
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button type="button" className="orange">
+      <button type="button" onClick={handleClick} name="1">
+        1
+      </button>
+      <button type="button" onClick={handleClick} name="2">
+        2
+      </button>
+      <button type="button" onClick={handleClick} name="3">
+        3
+      </button>
+      <button type="button" className="orange" onClick={handleClick} name="+">
         +
       </button>
-      <button type="button" className="span-two">
+      <button type="button" className="span-two" onClick={handleClick} name="0">
         0
       </button>
-      <button type="button">+</button>
-      <button type="button" className="orange">
+      <button type="button" onClick={handleClick} name=".">
+        .
+      </button>
+      <button type="button" className="orange" onClick={handleClick} name="=">
         =
       </button>
     </div>
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,3 +1,14 @@
-export default function Display() {
-  return <div className="output" />;
+import React from 'react';
+
+export default function Display(prop) {
+  const { value } = prop;
+  return (
+    <div className="output">
+      <p>
+        {value.total}
+        {value.operation}
+        {value.next}
+      </p>
+    </div>
+  );
 }
